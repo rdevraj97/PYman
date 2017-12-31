ba6b5ba55a4cc4a72b8bf4bcf2574560ee74562d
Rohan Devraj <rdevraj97@gmail.com>
2017-12-30 21:01:01 -0800

    Add files via upload

--- pyman.py
+++ pyman.py
@@ -1,5 +1,6 @@
 # Welcome to a Python variation of the popular Hangman game: PYman; which revolves
-# around thorough interaction with the Python console.
+# around thorough interaction with the Python console. The words the user has to guess
+# are only animals
 
 import time # Helps to delay output in the console 
 
@@ -14,37 +15,38 @@ before proceeding to play the game. Enjoy!")
 time.sleep(1)
 
 # RULES
-# 1. Words that are either a name or place start with a capital letter!
-# 2. Words can only be guessed letter by letter
-# 3. The word MUST be guessed before running out of the total attempts specified.
-# 4. When it asks to guess a letter, make sure to only input a single character
-# 5. Make sure you avoid guessing the same letter in any instance
+# 1. Words can only be guessed letter by letter.
+# 2. The word MUST be guessed before running out of the total attempts specified.
+# 3. When it asks to guess a letter, make sure to only input a single character.
+# 4. Make sure you avoid guessing the same letter in any instance
+# 5. All letters are in lowercase.
 
 
 
 ###########################################################################
 
-import urllib.request # extracts the words from the website and puts them in a list
 import random # helps us get a random number to use as an index in the words_list
 
+animal_file = open("animals.txt", "r")
 
+animals_lst = [] 
+# initializes an empty list, to store all the animal words after reading the file
 
-words_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+for line in animal_file:
+    line = line.replace('\n', '')
+    animals_lst.append(line)
+    
+animal_file.close()
 
-read_words = urllib.request.urlopen(words_site)
-site_txt = read_words.read()
+# there are 52 words in animals_lst to guess from
+random_index = random.randint(0, 51)
 
-all_words = site_txt.splitlines()
+animal_word = animals_lst[random_index]
+# a random word is selected each time the game is run from the animals_lst list
+print(animal_word)
 
-# there are 25487 words in all_words extracted from the word bank in the website
-random_index = random.randint(0, 25486)
-
-original_word = all_words[random_index].decode("utf-8")
-# a random word is selected each time the game is run from the all_words list
-# converts original_word from bytes to a string using .decode() method
-print(original_word)
-
-length = len(original_word)
+length = len(animal_word) 
+print(length)
 # string length of the word selected
 
 fails = length * 2 # Number of attempts to guess the word
@@ -57,7 +59,7 @@ all_guesses = '' # will include the letters guessed
 while (fails > 0):
     wrong_count = 0 # keeps track of the number of incorrect guesses
     
-    for letter in original_word:
+    for letter in animal_word:
         if str(letter) in all_guesses:
             print (letter)
         else:
@@ -67,19 +69,21 @@ while (fails > 0):
     if wrong_count == 0:
         break
     
-    guess = input("Guess a letter: ")
+    guess = input("Guess a letter or the entire word if you know it: ")
+    if (guess == animal_word):
+        break
     all_guesses += guess
     
-    if guess not in original_word:
+    if guess not in animal_word:
         print("The letter: " + guess + " is not in the word")
         fails = fails - 1
         print("You have",fails,"turns left to guess the word!")
 
 if (fails == 0):
-    print("You have not guessed the word, the word was " + original_word + \
-          ". Play again!")
+    print("You have not guessed the word, the word was " + animal_word + \
+          ". Try another word!") #Losing case
 else:
-    print ("Congrats! You have successfully guessed the word!")
+    print ("Congrats! You have successfully guessed the word!") #Winning case
 
         
         
